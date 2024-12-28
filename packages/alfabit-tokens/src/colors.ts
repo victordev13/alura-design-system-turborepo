29b3008baf2167900babd23943033308f92d3451
victordev13 <vtrcarvalho.13@gmail.com>
2024-12-28 13:39:16 -0300

    feat: more colors

--- packages/alfabit-tokens/src/colors.ts
+++ packages/alfabit-tokens/src/colors.ts
@@ -1,4 +1,34 @@
 export const colors = {
-  bgLight: '#ffffff',
-  bgDark: '#f8f8f8',
-}
+  background: {
+    bgLight: '#ffffff',
+    bgDark: '#f8f8f8',
+  },
+  primary: '#2d5bff',
+
+  white: '#FFFFFF',
+  black: '#000000',
+
+  gray: {
+    primary: '#181818',
+    secondary: '#5c5c5c',
+    tertiary: '#747474',
+    icon: '#7c7b7b',
+    border: '#d7d7d7',
+    divider: '#ededed',
+    disabled: '#8a8a8a',
+  },
+  blue: {
+    primary: '#2d5bff',
+    secondary: '#6284fd',
+    tertiary: '#96ADFF',
+    quaternary: '#ECF0FF',
+    hover: '#1B4AF0',
+    click: '#002ED0',
+  },
+  violet: {
+    primary: '#AF4BFE',
+    secondary: '#BE74F9',
+    tertiary: '#E2BDFF',
+    quaternary: '#F5E8FF',
+  },
+} as const
